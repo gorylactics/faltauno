6d7aebb2d914a6d2a2afb45a06cdaa05b531c40c
adrian <adrian.miranda.r@gmail.com>
2023-11-20 17:09:49 -0300

    creacion de perfiles , menu , muro principal ((solo front y un poco de logica de direccionamiento , no hay validaciones por el momento)

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views as views_core
 
 urlpatterns = [
-    path('' , views_core.index , name='index'),
+    path('' , views_core.index , name='index'), 
+    path('login/' , views_core.login , name='login'),
+    path('success/' , views_core.success , name='success'),
+    path('perfil_jugador/' , views_core.perfil_jugador , name='perfil_jugador'),
+    path('perfil_centro/' , views_core.perfil_centro , name='perfil_centro'),
+    path('muro_principal' , views_core.muro_principal , name="muro_principal")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render , redirect
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     contexto = {'titulo' : 'Registro | Login'}
     if request.method == 'GET':
         return render(request, 'core/index.html' , contexto)
+
+
+def login(request):
+    contexto = {'titulo' : 'Logeando'}
+    if request.method == 'GET':
+        return redirect('/')
+    if request.method == 'POST':
+        correo = request.POST['log_in_usuario']
+        clave = request.POST['pass_usuario']
+        print(correo  , clave)
+        return redirect('/success/')
+
+def success(request):
+    valor = request.POST['jugador']
+    print(valor)
+    return redirect('/perfil_jugador/')
+
+
+def perfil_jugador(request):
+    contexto = {'titulo' : 'Perfil Jugador'}
+    return render(request , 'core/perfil_jugador.html' , contexto)
+
+def perfil_centro(request):
+    contexto = {'titulo' : 'Perfil Centro'}
+    return render(request , 'core/perfil_centro.html' , contexto)
+
+def muro_principal(request):
+    return render(request , 'core/muro_principal.html')
